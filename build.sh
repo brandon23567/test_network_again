0dcb5d0399a7dd6d30cdbc5b412f0a7f6319452d
brandon23567 <brandonmahuvava0@gmail.com>
2024-01-09 23:43:04 +0530

    now im just fucking around until something works

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip3 install -r requirements.txt
 
-python3.12 manage.py collectstatic --no-input
-python3.12 manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
